92515417d8d72acae0b2cd4033cda7d5541d48b9
zuri227 <zuri22780@gmail.com>
2022-04-05 22:11:25 -0400

    Solve exercises 1 & 2

---
+++ python-functions.py
@@ -0,0 +1,21 @@
+#1. Write a function named sum_to that accepts a single integer, n, and returns the sum of the integers from 1 to n.
+
+# def sum_to(n):
+#   sum = 0
+#   for i in range(1, n+1):
+#     sum += i
+#     print(sum)
+
+# sum_to(6)
+
+#2. Write a function named largest that takes a list of numbers as an argument and returns the largest number in that list.
+
+def largest(nums):
+  x = max(nums)
+  print(x)
+
+largest([25, 6, 70])
+
+  
+
+
