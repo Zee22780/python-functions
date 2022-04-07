d75f5615852c6b8d55707d5bc7f0fed87eacea90
zuri227 <zuri22780@gmail.com>
2022-04-06 21:07:11 -0400

    Solve exercise 3

--- python-functions.py
+++ python-functions.py
@@ -1,12 +1,12 @@
 #1. Write a function named sum_to that accepts a single integer, n, and returns the sum of the integers from 1 to n.
 
-# def sum_to(n):
-#   sum = 0
-#   for i in range(1, n+1):
-#     sum += i
-#     print(sum)
+def sum_to(n):
+  sum = 0
+  for i in range(1, n+1):
+    sum += i
+    print(sum)
 
-# sum_to(6)
+sum_to(6)
 
 #2. Write a function named largest that takes a list of numbers as an argument and returns the largest number in that list.
 
@@ -16,6 +16,15 @@ def largest(nums):
 
 largest([25, 6, 70])
 
-  
+#3. Write a function named occurrences that takes two string arguments as input and counts the number of occurrences of the second string inside the first string.
+
+def occurrences(one, two):
+  print(one.count(two))
+
+occurrences('fleep floop', 'e')
+
+
+
+
 
 
